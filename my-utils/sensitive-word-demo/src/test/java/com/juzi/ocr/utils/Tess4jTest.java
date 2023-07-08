6c1b190d98f6c9bf37ff5a670a70fc7fd09e2638
CodeJuzi <d1741530592@163.com>
2023-07-08 16:04:38 +0800

    feat: add tess4j ocr

---
+++ my-utils/sensitive-word-demo/src/test/java/com/juzi/ocr/utils/Tess4jTest.java
@@ -0,0 +1,34 @@
+package com.juzi.ocr.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+
+import java.io.File;
+
+/**
+ * @author codejuzi
+ */
+@Slf4j
+public class Tess4jTest {
+    public static void main(String[] args) {
+        String path = "/Users/codejuzi/Documents/CodeWorkSpace/Practice/Prac-Demo/my-utils/sensitive-word-demo/src/main/resources/image-20210524161243572.png";
+        try {
+            //获取本地图片
+            File file = new File(path);
+            //创建Tesseract对象
+            ITesseract tesseract = new Tesseract();
+            //设置字体库路径
+            tesseract.setDatapath("classpath:/tessdata");
+            //中文识别
+            tesseract.setLanguage("chi_sim");
+            //执行ocr识别
+            String result = tesseract.doOCR(file);
+            //替换回车和tal键  使结果为一行
+            result = result.replaceAll("[\\r\\n]", "-").replaceAll(" ", "");
+            System.out.println("识别的结果为：" + result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
